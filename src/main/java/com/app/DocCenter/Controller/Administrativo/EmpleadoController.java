a55fa02b15f59683084e467d4453761b3db9b54a
Cristian Jesús Huamán Cruz <huamancruzcristianjesus@gmail.com>
2026-03-31 14:08:01 -0500

    @Rescontroller agregado

--- src/main/java/com/app/DocCenter/Controller/Administrativo/EmpleadoController.java
+++ src/main/java/com/app/DocCenter/Controller/Administrativo/EmpleadoController.java
@@ -1,4 +1,9 @@
 package com.app.DocCenter.Controller.Administrativo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class EmpleadoController {
 }
