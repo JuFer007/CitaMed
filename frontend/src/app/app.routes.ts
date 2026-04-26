06f2adaa25707ed008d3514fd23d035d2f5cdab4
Cristian Jesús Huamán Cruz <huamancruzcristianjesus@gmail.com>
2026-04-25 21:33:42 -0500

    Feature: Angular frontend
    Se agregó el proyecto de angular

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,46 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login-component/login-component';
+import { HomeComponent } from './features/home/home-component';
+import { ProductosPage } from './features/home/productos-page/productos-page';
+import { ServiciosPage } from './features/home/servicios-page/servicios-page';
+import { DashboardLayout } from './features/admin/dashboard-layout/dashboard-layout';
+import { UsuarioComponent } from './features/admin/usuario-component/usuario-component';
+import { DashboardComponent } from './features/admin/dashboard-component/dashboard-component';
+import { VentasComponent } from './features/admin/ventas-component/ventas-component';
+import { authGuard } from './core/guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    component: HomeComponent,
+    title: 'Acuamont - Bienvenidos',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Iniciar Sesión',
+  },
+  {
+    path: 'ver-productos',
+    component: ProductosPage,
+    title: 'Productos',
+  },
+  {
+    path: 'ver-servicios',
+    component: ServiciosPage,
+    title: 'Servicios',
+  },
+  {
+    path: 'admin',
+    component: DashboardLayout,
+    title: 'Dashboard',
+    canActivate: [authGuard],
+    children: [
+      { path: 'usuarios', component: UsuarioComponent, data: { title: 'Usuarios' } },
+      { path: 'dashboard', component: DashboardComponent, data: { title: 'Dashboard' } },
+      { path: 'ventas', component: VentasComponent, data: { title: 'Ventas' } }
+    ]
+  },
+  { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+  { path: '**', redirectTo: 'inicio' },
+];
