5ab52bb227e56ca1fea4363b3d0ae28a743606ca
Cristian Jesús Huamán Cruz <huamancruzcristianjesus@gmail.com>
2026-06-23 17:23:15 -0500

    Update h2

--- backend/src/test/java/com/app/CitaMed/DocCenterApplicationTests.java
+++ backend/src/test/java/com/app/CitaMed/DocCenterApplicationTests.java
@@ -2,8 +2,10 @@ package com.app.CitaMed;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DocCenterApplicationTests {
 
 	@Test
